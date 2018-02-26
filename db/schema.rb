25c61388d022dfc210e32b7d9d3d4bca02384371
Moe yajima <yajima_moe@nexway.co.jp>
2018-02-26 04:14:10 +0000

    commit

---
+++ db/schema.rb
@@ -0,0 +1,12 @@
+ActiveRecord::Schema.define(version: 20180225084558) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.string "email"
+    t.text "content"
+  end
+
+end
