1808794ecbc12b50f5933d516c526d4b2c062834
Moe yajima <yajima_moe@nexway.co.jp>
2018-02-27 05:54:36 +0000

    first commit

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,5 +1,5 @@
 class Blog < ApplicationRecord
-    validates :title,presence: true
+    validates :title,presence: true,length: { in: 1..140 }
     validates :email, presence: true,length: { in: 1..140 }
     validates :content, presence: true,length: { in: 1..140 }
 end
