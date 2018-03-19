cfde222d8f4f5a2f633e09ba60a36374f55e17dc
Moe yajima <yajima_moe@nexway.co.jp>
2018-03-19 06:55:40 +0000

    Footer修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :contacts
   resources :feeds
   resources :blogs do
      collection do
@@ -14,5 +15,9 @@ Rails.application.routes.draw do
   resources :users
   resources :favorites, only: [:create, :destroy]
   
+  if Rails.env.development?
+  mount LetterOpenerWeb::Engine, at: "/letter_opener"
+end
+
 end
 
