69f003e701107058b653b10e22cb9eef686cf134
Moe yajima <yajima_moe@nexway.co.jp>
2018-03-19 08:45:36 +0000

    メーラー編集

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,8 +1,6 @@
 class ContactMailer < ApplicationMailer
-
   def contact_mail(contact)
     @contact = contact
-    mail to:"yajima_moe@nexway.co.jp",subject: "お問い合わせの確認メール"
-　end
-end
+    mail to: "yajima_moe@nexway.co.jp", subject: "お問い合わせの確認メール"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,9 +15,9 @@ Rails.application.routes.draw do
   resources :users
   resources :favorites, only: [:create, :destroy]
   
-  #if Rails.env.development?
- # mount LetterOpenerWeb::Engine, at: "/letter_opener"
-#  end
+  if Rails.env.development?
+  mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
 
 end
 
