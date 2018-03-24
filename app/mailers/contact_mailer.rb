e9c9c43f2e5b6504223cd7f0b8ea8982d83deab2
Moe yajima <yajima_moe@nexway.co.jp>
2018-03-24 14:56:42 +0000

    メーラー作成

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,6 @@
 class ContactMailer < ApplicationMailer
   def contact_mail(contact)
     @contact = contact
-    mail to: "sillber426.trs2@i.softbank.jp", subject: "お問い合わせの確認メール"
+    mail to: "@contact.email", subject: "お問い合わせの確認メール"
   end
 end
