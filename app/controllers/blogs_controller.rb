5d28d6be1500d51a7834c187b504987e6ac5c52b
Moe yajima <yajima_moe@nexway.co.jp>
2018-03-26 08:28:31 +0000

    rename Blogscontroller

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,4 +1,4 @@
-class blogController < ApplicationController
+class BlogsController < ApplicationController
   before_action :require_login
   before_action :set_blog, only:[:show, :edit, :update, :destroy]
   
